af62e48aba637f647c7bbb5e305cfcfb1105a195
BaranBey1331 <152785244+BaranBey1331@users.noreply.github.com>
2026-03-02 16:01:55 +0300

    Create config.h

---
+++ src/H files/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Language Metadata
+#define VOLANG_NAME "VoLang"
+#define VOLANG_VERSION "0.1.0"
+
+// The current active file extension (change to ".v", ".vol", ".vopp" as needed)
+#define VOLANG_EXT ".vo"
+
+#endif // CONFIG_H
