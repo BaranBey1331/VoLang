2d429c7f29ef774cb7930fa20f18e16b05657ee2
BaranBey1331 <152785244+BaranBey1331@users.noreply.github.com>
2026-03-02 22:12:51 +0300

    Update token.h

--- src/H files/token.h
+++ src/H files/token.h
@@ -3,21 +3,20 @@
 
 #include <stddef.h>
 
-// Token types for Volang
 typedef enum {
     TOKEN_ILLEGAL,
     TOKEN_EOF,
 
-    // Identifiers and literals
-    TOKEN_IDENT, // Variables, function names
-    TOKEN_INT,   // 12345
+    TOKEN_IDENT, 
+    TOKEN_INT,   
 
-    // Operators
     TOKEN_ASSIGN, // =
     TOKEN_PLUS,   // +
     TOKEN_MINUS,  // -
+    TOKEN_ASTERISK, // *
+    TOKEN_SLASH,    // /
+    TOKEN_PERCENT,  // %
 
-    // Delimiters
     TOKEN_COMMA,     // ,
     TOKEN_SEMICOLON, // ;
     TOKEN_LPAREN,    // (
@@ -25,18 +24,16 @@ typedef enum {
     TOKEN_LBRACE,    // {
     TOKEN_RBRACE,    // }
 
-    // Keywords (English base, Turkish will be added later)
     TOKEN_LET,
     TOKEN_FN,
     TOKEN_RETURN
 } TokenType;
 
-// Zero-copy Token structure
 typedef struct {
     TokenType type;
-    const char* literal; // Pointer to the start of the token in the source file
-    size_t length;       // Length of the token
-    int line;            // Line number for error reporting
+    const char* literal;
+    size_t length;
+    int line;
 } Token;
 
 #endif // TOKEN_H
